dd36186193b75e6888fd6a4b3e380732d4ee6982
Mentor Ibrahimi <mibrahimi@cpaglobal.com>
2019-07-22 15:03:56 +0200

    Added participation entity
    Updated seed data
    Fixed length issue in EventValidator
    Updated NuGet packages
    Removed duplicated ViewImports

---
+++ src/Gram/Gram.Core/Entities/Participation.cs
@@ -0,0 +1,20 @@
+﻿using Gram.Core.Interfaces;
+using System;
+
+namespace Gram.Core.Entities
+{
+    public class Participation : IEntity
+    {
+        public int Id { get; set; }
+        public int EventId { get; set; }
+        public int PersonId { get; set; }
+        public int StatusId { get; set; }
+        public DateTime StatusDate { get; set; }
+        public string Remarks { get; set; }
+        public byte[] RowVersion { get; set; }
+
+        public virtual Event Event { get; set; }
+        public virtual Person Person { get; set; }
+        public virtual GeneralType Status { get; set; }
+    }
+}
---
+++ src/Gram/Gram.Infrastructure/EntityConfigurations/ParticipationConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Gram.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace Gram.Infrastructure.EntityConfigurations
+{
+    public class ParticipationConfiguration : IEntityTypeConfiguration<Participation>
+    {
+        public void Configure(EntityTypeBuilder<Participation> builder)
+        {
+            builder.ToTable("Participation", "Events");
+            builder.Property(m => m.StatusDate)
+                .HasColumnType("date")
+                .HasDefaultValue(DateTime.UtcNow)
+                .IsRequired();
+            builder.Property(m => m.Remarks)
+                .HasMaxLength(50);
+            builder.HasIndex(m => m.EventId)
+                .HasName("IX_Participation_Event");
+            builder.HasOne(m => m.Event)
+                .WithMany(m => m.EventParticipations)
+                .HasForeignKey(m => m.EventId);
+            builder.HasIndex(m => m.PersonId)
+                .HasName("IX_Participation_Person");
+            builder.HasOne(m => m.Person)
+                .WithMany(m => m.ParticipatingPeople)
+                .HasForeignKey(m => m.PersonId);
+            builder.HasIndex(m => m.StatusId)
+                .HasName("IX_Participation_Status");
+            builder.HasOne(m => m.Status)
+                .WithMany(m => m.ParticipationStatus)
+                .HasForeignKey(m => m.StatusId);
+        }
+    }
+}
