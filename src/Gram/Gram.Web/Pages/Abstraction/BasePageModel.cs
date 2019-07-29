e800e89e06dc22dbe6b949ec0963fe95c63d39b5
Mentor Ibrahimi <mentortori@hotmail.com>
2019-07-29 22:27:50 +0200

    Added authorization requirement

--- src/Gram/Gram.Web/Pages/Abstraction/BasePageModel.cs
+++ src/Gram/Gram.Web/Pages/Abstraction/BasePageModel.cs
@@ -1,10 +1,12 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Gram.Web.Pages.Abstraction
 {
+    [Authorize]
     public abstract class BasePageModel : PageModel
     {
         private IMediator _mediator;
