ee3611fb8d583bead4489839660e4b14e0fb1261
Mentor Ibrahimi <mibrahimi@cpaglobal.com>
2019-08-12 09:12:55 +0200

    Added script to recreate database

---
+++ src/Scripts/Recreate database.ps1
@@ -0,0 +1,6 @@
+﻿dotnet ef database drop --force --project ..\Gram\Gram.Web\ --context IdentityContext
+dotnet ef database update --project ..\Gram\Gram.Web\ --context IdentityContext
+dotnet ef database update --project ..\Gram\Gram.Web\ --context AuditContext
+dotnet ef database update --project ..\Gram\Gram.Web\ --context DataContext
+Invoke-Sqlcmd -InputFile ".\Create foreign key constraint.sql" -ServerInstance "(localdb)\mssqllocaldb"
+Invoke-Sqlcmd -InputFile ".\Seed data.sql" -ServerInstance "(localdb)\mssqllocaldb"
