d99670a083c27a24d331a5e047d15ed69feab9fd
Mentor Ibrahimi <mentortori@hotmail.com>
2019-08-16 21:36:23 +0200

    Add guides

---
+++ src/Gram/Gram.Domain/Entities/EventGuide.cs
@@ -0,0 +1,15 @@
+﻿using Gram.Domain.Interfaces;
+
+namespace Gram.Domain.Entities
+{
+    public sealed class EventGuide : IEntity
+    {
+        public int Id { get; set; }
+        public int EventId { get; set; }
+        public int GuideId { get; set; }
+        public byte[] RowVersion { get; set; }
+
+        public Event Event { get; set; }
+        public Guide Guide { get; set; }
+    }
+}
---
+++ src/Gram/Gram.Domain/Entities/Guide.cs
@@ -0,0 +1,22 @@
+﻿using Gram.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace Gram.Domain.Entities
+{
+    public sealed class Guide : IEntity
+    {
+        public Guide()
+        {
+            EventGuides = new HashSet<EventGuide>();
+        }
+
+        public int Id { get; set; }
+        public int PersonId { get; set; }
+        public bool IsActive { get; set; }
+        public byte[] RowVersion { get; set; }
+
+        public Person Person { get; set; }
+        public ICollection<EventGuide> EventGuides { get; }
+    }
+}
